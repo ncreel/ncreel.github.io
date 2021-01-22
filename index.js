6f8a737b4ac9683f86350405b94ce022784753be
Nick <31663172+ncreel@users.noreply.github.com>
2021-01-22 14:03:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var aboutme = '<p>M. Creel (they/he/she, f.k.a. Nick Creel) is a genderfucked poet studying and working as an MFA candidate at the University \
                of Massachusetts, Boston. They work as a poetry editor for <i>Breakwater Review</i> and as an undergraduate \
                writing tutor. They are fascinated by the magic of language, both human and computer, and are interested \
-               in writing and reading electronic and visual poetry that is unapologetically cringe.</p>\
-               <p><em>creel</em><br/> /krēl/<br/> noun<br> 1. a wicker basket for carrying fish.<br/>2. a rack holding bobbins or spools for spinning.</p>\'
-
+               in writing and reading electronic and visual poetry that is unapologetically cringe.</p>\'
+            
 var poetry = '<h3>2021</h3>\
           <ul>\
             <li> "Repeated Repentance," "Degenerate Gods," "Gunshot" - <i>SPORAZINE</i> (Forthcoming) </li> \
